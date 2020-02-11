3f1500d5e7afbe84452fba88002351528859f860
Jack Cai <cwx8900@gmail.com>
2020-02-12 00:43:28 +0800

    update application name and server port

---
+++ src/main/java/com/eurekaclient/DateServiceController.java
@@ -0,0 +1,15 @@
+package com.eurekaclient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DateServiceController {
+
+    @RequestMapping( value = "/test", method = RequestMethod.GET )
+    public String test( @RequestParam String param ){
+        return "hello " + param;
+    }
+}
--- src/main/java/com/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/eurekaclient/EurekaClientApplication.java
@@ -8,14 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Jack
  *
- * 服务1
+ * 创建服务提供者
  * Instances currently registered with Eureka
- * UNKNOWN	n/a (1)	(1)	UP (1) - 192.168.XX.XX
+ * SERVICE_PROVIDER	n/a (1)	(1)	UP (1) - 192.168.11.38:service_provider:9001
  *
  * registered-replicas	http://eureka-7003.com:7003/eureka/, http://eureka-7002.com:7002/eureka/
  * unavailable-replicas	http://eureka-7003.com:7003/eureka/,http://eureka-7002.com:7002/eureka/,
  * available-replicas
- *
  */
 @SpringBootApplication
 @EnableDiscoveryClient
